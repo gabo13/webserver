02f6a7cdf04f875bf2d8a0d9c4be18564f98582f
gabo13 <horvathg83@gmail.com>
2024-05-17 14:12:15 +0200

    lotto

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import importlib
 import pkgutil
 import plugins
-from flask import Flask, send_from_directory, render_template
+from flask import Flask, send_from_directory, render_template, jsonify, request
+import time
 from flask_cors import CORS
 print(f'{__name__} loaded')
 
@@ -33,9 +34,60 @@ def index():
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(app.static_folder, 'images/favicon.ico', mimetype='image/vnd.microsoft.icon')
+###############
+#
+# Lotto api
+#
+lotto_file_access = False
+filename = "/home/pi/AUTOMAT/lotto/emailforall.txt"
 
 
+def read_subscribers():
+    global lotto_file_access
+    while lotto_file_access:
+        time.sleep(1)
+    lotto_file_access = True
+    with open(filename,"rt") as f:
+        subscribers = [email.strip() for email in f]
+        lotto_file_access = False
+        return subscribers
 
+
+def write_subscribers(subscribers):
+    global lotto_file_access
+    while lotto_file_access:
+        time.sleep(1)
+    lotto_file_access = True
+    with open(filename,"wt") as f:
+        for email in subscribers:
+            f.write(email+"\n")
+        lotto_file_access =False
+
+
+   
+@app.route('/lotto/subscribe/')
+def lotto():
+    return render_template("lottosubscribe.html")
+
+@app.route('/lotto/subscribe/api', methods=["POST"])
+def lottoapi():
+    json_data = request.get_json()
+    email = json_data.get("email")
+    subscribers = read_subscribers()
+    if email in subscribers:
+        print("leiratkozas", subscribers)
+        subscribers.remove(email)
+        write_subscribers(subscribers)
+        return jsonify({"msg": "Sikeresen leiratkoztál!"})
+    else:
+        print("leiratkozas", subscribers)
+        subscribers.append(email)
+        write_subscribers(subscribers)
+        return jsonify({"msg": "Sikeres feliratkozás!"})
+#
+# Lotto api end
+#
+####################
 rules = [rule.rule for rule in app.url_map.iter_rules()]
 rules.sort()
 print("*****RULES: *****")
