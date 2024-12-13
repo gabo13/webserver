8516e0f5897b6920ff17e4a1757aec3f4d87a3ab
gabo13 <horvatg83@gmail.com>
2024-12-13 20:53:44 +0100

    update241213

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 https://nts.strzibny.name/systemd-user-services/
 sudo setcap 'CAP_NET_BIND_SERVICE+ep' /usr/bin/netcat
-
+~/.config/systemd/user/
 installed modules:
 - flask
 
--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-
 export APP_PATH=$(realpath $(dirname $0))
 export SHOPPING_LIST="$APP_PATH/data/shopping_list.json"
 export FLASK_APP="$APP_PATH/app.py"
+export FLASK_VENV="/home/gabo13/Tools/venv/bin/flask"
 echo "Script path: $APP_PATH"
 if  [ $USER == gabo13 ]
 then
     export FLASK_DEBUG=False
-    $APP_PATH/venv/bin/flask run --host=0.0.0.0 --port=80
+    sudo setcap 'cap_net_bind_service=+ep' "$(readlink -f $(which python))"
+    $FLASK_VENV run --host=0.0.0.0 --port=80
 else
     export FLASK_DEBUG=True
-    $APP_PATH/venv/bin/flask run --host=0.0.0.0 --port=8080
+    $FLASK_VENV --host=0.0.0.0 --port=8080
 fi
 
